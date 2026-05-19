9e885c8cb544400f2968aadeb1fa3753f0b9f1b5
sreeharshadonthi <sreeharshadonthi@gmail.com>
2026-05-19 13:19:38 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,270 @@
+// 1. Create a variable using var and store your name.
+
+var name="Sree Harsha";
+console.log(name);
+
+// 2. Print your name using console.log().
+
+console.log("Sree Harsha");
+
+// 3. Create a variable using let and store your age.
+
+let age=23;
+
+// 4. Change the value of age and print it.
+
+age=24;
+console.log(age);
+
+// 5. Create a constant variable for country name.
+
+const country="India";
+
+// 6. Print the country name.
+
+console.log(country);
+
+// 7. Try redeclaring var variable and observe output.
+
+var h=10;
+var h=40;
+console.log(h); //40
+
+// 8. Try redeclaring let variable and observe error.
+
+let i=9;
+let i=4; //not possible
+console.log(i); //SyntaxError
+
+// 9. Try reassigning const variable and observe error.
+
+const j=13;
+j=89; //not possible
+console.log(j);
+
+// 10. Write difference between var and let.
+
+//=======var======                           =====let=====
+
+// i) Its a local and function scope         i) It is block scope
+// ii) Redeclaration is possible              ii) Redeclaration is not possible                                        
+
+// 11. Write difference between let and const.
+
+//=======let======                           =====const=====
+// i) Re-initailzation is possible          i) Re-initialization is not possible
+
+// 12. Predict output
+
+var a = 1
+    a = 2
+var a = 45
+
+console.log(a) //45
+
+
+// 13. Predict output
+
+let b = 56
+    b = 98
+
+console.log(b)//98
+
+
+// 14. Predict output
+
+const c = 45 
+
+console.log(c) //45
+
+
+// 15. Use console.log() to print "Hello World".
+
+console.log("Hello World")
+
+// 16. Use alert() to show "Welcome User".
+
+alert("Welcome User");
+
+// 17. Use confirm() to ask user:
+// "Do you like JavaScript?"
+
+confirm("Do you like JavaScript?");
+
+// 18. Use prompt() to ask user name.
+
+let n=prompt("What is your name?")
+
+// 19. Print prompt value in console.
+
+console.log(n);
+
+
+// 20. Use document.writeln() to print "Batch 33".
+
+document.writeln("Batch 33")
+
+// 21. Create variable studentName and store your name.
+
+var studentName="Arfath";
+
+// 22. Create variable studentAge and store your age.
+
+var studentAge=23;
+
+// 23. Print both variables together.
+
+console.log(studentName+" "+studentAge);
+
+// 24. Create a variable for mobile number.
+
+var mobileNumber=7013497213;
+
+// 25. Print mobile number using console.log().
+
+console.log(mobileNumber);
+
+// 26. Create variable using var for favorite color.
+
+var favColor="Green"
+
+// 27. Reassign another color value.
+
+favColor="Blue";
+
+// 28. Create variable using let for marks.
+
+let mark=97;
+
+// 29. Change marks value and print it.
+
+mark=90;
+console.log(mark);
+
+// 30. Create const variable for PI value.
+
+const value=3.14;
+
+// 31. Print PI value.
+
+console.log(value);
+
+// 32. Predict output
+
+var x = 5
+var x = 15
+
+console.log(x)//15
+
+
+// 33. Predict output
+
+let y = 25
+    y = 35
+
+console.log(y)//35
+
+
+// 34. Find error
+
+const z = 40
+      z = 45 //not possible
+
+console.log(z) //(TypeError) re-initialization is not possible
+
+// 35. Create alert box with your college name.
+
+alert("Gokaraju Rangaraju Institute of Engineering and Tech")
+
+// 36. Ask user age using prompt().
+
+let ageNew=prompt("Enter your Age?")
+
+// 37. Print user age in console.
+
+console.log(ageNew);
+
+// 38. Ask user confirmation:
+// "Did you complete HTML?"
+
+confirm("Did you complete HTML?")
+
+// 39. Print confirm result in console.
+if(true)
+{
+    console.log("Completed")
+}
+else
+{
+    console.log("Not Completed");
+}
+
+// 40. Print "JavaScript Class" using document.writeln().
+
+document.writeln("JavaScript Class")
+
+// 41. Create 3 variables:
+// city, state, country.
+
+var city="Hyderabad"
+var state="Telangana"
+var country1="India"
+
+// 42. Print all variables together.
+
+console.log(city+" "+state+" "+country1);
+
+// 43. Create one example for var.
+
+var f=66;
+    f=54;
+var g=24;
+console.log(f+g);
+
+// 44. Create one example for let.
+
+let r=26;
+    r=28;
+let t=23;
+console.log(r-t);
+
+// 45. Create one example for const.
+
+const u=5;
+// g=6; not possible
+console.log(u);
+
+// 46. Predict output
+
+var num = 100
+    num = 200
+
+console.log(num) //200
+
+
+// 47. Predict output
+
+let price = 500
+    price = 700
+console.log(price)//700
+
+
+// 48. Predict output
+
+const password = 12345
+
+console.log(password)//12345
+
+
+// 49. Write rules of var.
+
+// i) We can declare and initialize
+// ii) Re-initailization is possible
+// ii) Re-declaration is also possible
+
+// 50. Write rules of let and const.
+
+// ========let======                          ======const=======
+// i)We can declare and initialize            i)We can declare and initialize
+// ii)Re-initialization is possible           ii)Re-initialization is not possible
+// iii)Re-declaration is not possible         iii)Re-declaration is not possible
