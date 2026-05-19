1a7a2ec22134e925f63848ab4769bab766549c6f
sreeharshadonthi <sreeharshadonthi@gmail.com>
2026-05-19 13:31:35 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,187 @@
+// 1. Create a variable and store your favorite food.
+    
+      let favoriteFood = "Biryani";
+
+// 2. Create a variable and store your mobile number.
+      
+      let mobileNumber = 7013497213;
+
+// 3. Create a boolean variable with false value.
+
+      let False = false;
+
+// 4. Create one undefined variable and print typeof.
+      let main;
+      console.log(typeof main);
+
+// 5. Create one array with 6 animal names.
+      
+      let animal = ["Lynx", "Cat", "Sheep", "Dog", "Fox", "Lion"];
+      
+
+// 6. Print second animal from array.
+
+      console.log(animal[1]);
+
+// 7. Print last animal from array using length.
+
+      console.log(animal[animal.length - 1]);
+
+// 8. Create object with bike details (name, model, color).
+
+      let bike = { name: "Triumph", model: "765R", color: "Yellow" };
+
+// 9. Print bike color from object.
+
+      console.log(bike.color);
+
+
+// 10. Create two variables and subtract values.
+
+       let a = 100;
+       let b = 50;
+       console.log(a - b);
+
+// 11. Create two variables and divide values.
+
+       let c = 50;
+       let d = 2;
+       console.log(c / d);
+
+// 12. Find 15 % 2.
+
+       console.log(15 % 2);
+
+// 13. Find 2 ** 5.
+
+       console.log(2 ** 5);
+
+// 14. Create variable with 5 and use post increment.
+    
+       let x = 5;
+       console.log(x++);
+       console.log(x);
+
+// 15. Create variable with 10 and use pre increment.
+
+       let y = 10;
+       console.log(++y);
+
+// 16. Create variable with 20 and use post decrement.
+
+       let z = 20;
+       console.log(z--);
+       console.log(z); 
+
+// 17. Create variable with 50 and use pre decrement.
+
+       let p = 50;
+       console.log(--p);
+
+// 18. Check 100 > 50.
+
+       console.log(100 > 50);
+
+// 19. Check 25 < 10.
+
+       console.log(25 < 10);
+
+// 20. Check 50 >= 50.
+       
+       console.log(50 >= 50);
+
+// 21. Check 30 <= 25.
+       
+       console.log(30 <= 25);
+
+// 22. Check 10 == "10".
+
+       console.log(10 == 10);
+
+// 23. Check 10 === "10".
+
+       console.log(10 === "10");       
+
+// 24. Check 25 != "25".
+
+       console.log(25 != "25");
+
+// 25. Check 25 !== "25".
+
+       console.log(25 !== "25");
+
+// 26. Use AND operator:
+//      10 > 5 && 20 > 15
+
+       console.log(10 > 5 && 20 > 15);
+
+// 27. Use AND operator:
+//     5 > 10 && 20 > 15
+
+       console.log(5 > 10 && 20 > 15);
+
+// 28. Use OR operator:
+//     5 > 10 || 20 > 15
+
+       console.log(5 > 10 || 20 > 15); 
+
+// 29. Use OR operator:
+//     2 > 5 || 1 > 10
+
+       console.log(2 > 5 || 1 > 10);
+
+// 30. Use NOT operator:
+//     !(10 > 5)
+
+       console.log(!(10 > 5));
+
+// 31. Use NOT operator:
+//     !(5 > 10)
+
+       console.log(!(5 > 10));
+
+// 32. Create ternary operator:
+//     if 50 > 25 print "True"
+//     else print "False"
+
+       console.log(50 > 25 ? "True" : "False");
+
+
+// 33. Create ternary operator:
+//     if 18 >= 21 print "Adult"
+//     else print "Minor"
+
+       console.log(18 >= 21 ? "Adult" : "Minor");
+
+// 34. Create one array with 5 city names and print third city.
+
+       let cities = ["Hyderabad", "Banglore", "Mumbai", "Chennai", "Delhi"];
+       console.log(cities[2]);    
+
+// 35. Create one object with employee details and print employee name.
+
+       let employee = { name: "Harsha", age: 25, id: "2288" };
+       console.log(employee.name);
+
+// 36. Create variable with string "100" and print typeof.
+
+       let Num = "100";
+       console.log(typeof Num);
+
+// 37. Create variable with number 100 and print typeof.
+
+       let num = 100;
+       console.log(typeof num);
+
+// 38. Create one null variable and print it.
+
+       let nullVar = null;
+       console.log(nullVar);
+
+// 39. Create one array with mixed datatypes.
+
+       let mixedArray = ["this", false, 76, null, { game: "valo" }];
+
+// 40. Create object with student details:
+
+       let student = { name: "Harsha", stu_id: 8789, house: red }
